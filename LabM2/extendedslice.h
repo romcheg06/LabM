ab42f16f403deb105e167b3831349c68b492fc8e
Author <reg_mail_info@inbox.ru>
2018-04-21 02:49:44 +0300

    Добавлена вторая лаба (Жизнь) с вьюером

---
+++ LabM2/extendedslice.h
@@ -0,0 +1,128 @@
+#ifndef EXTENDEDSLICE_H
+#define EXTENDEDSLICE_H
+
+#include "slice.h"
+
+/*!
+ * \brief расширенный кусок поля с границами и возможностью сделать один шаг игры
+ */
+struct ExtendedSlice
+{
+    explicit ExtendedSlice(Slice& slice):
+                m_slice(slice),
+                m_strideX{m_slice.m_stride},
+                m_strideY{m_slice.m_values.size() / m_strideX}
+    {
+        m_upperBound.resize(m_strideX, 0);
+        m_lowerBound.resize(m_strideX, 0);
+        m_leftExtendedBound.resize(m_strideY + 2, 0);
+        m_rightExtendedBound.resize(m_strideY + 2, 0);
+    }
+
+    values_t value(size_t x, size_t y) const
+    {
+        if(x == 0)
+            return m_leftExtendedBound[y];
+
+        if(x == m_strideX + 1)
+            return m_rightExtendedBound[y];
+
+        if(y == 0)
+            return m_upperBound[x - 1];
+
+        if(y == m_strideY + 1)
+            return m_lowerBound[x - 1];
+
+        return m_slice.m_values[(y - 1) * m_strideX + x - 1];
+    }
+
+    values_t& value(size_t x, size_t y)
+    {
+        if(x == 0)
+            return m_leftExtendedBound[y];
+
+        if(x == m_strideX + 1)
+            return m_rightExtendedBound[y];
+
+        if(y == 0)
+            return m_upperBound[x - 1];
+
+        if(y == m_strideY + 1)
+            return m_lowerBound[x - 1];
+
+        return m_slice.m_values[(y - 1) * m_strideX + x - 1];
+    }
+
+    std::vector<values_t> getUpperRow() const
+    {
+        return std::vector<values_t>{m_slice.m_values.begin(), m_slice.m_values.begin() + m_strideX};
+    }
+
+    std::vector<values_t> getLowerRow() const
+    {
+        return std::vector<values_t>{m_slice.m_values.end() - m_strideX, m_slice.m_values.end()};
+    }
+
+    std::vector<values_t> getFirstExtendedColumn() const
+    {
+        std::vector<values_t> column;
+        column.push_back(m_upperBound[0]);
+        for(size_t i = 0; i < m_strideY; ++i)
+            column.push_back(m_slice.m_values[i * m_strideX]);
+        column.push_back(m_lowerBound[0]);
+
+        return column;
+    }
+
+    std::vector<values_t> getLastExtendedColumn() const
+    {
+        std::vector<values_t> column;
+        column.push_back(m_upperBound[m_strideX - 1]);
+        for(size_t i = 0; i < m_strideY; ++i)
+            column.push_back(m_slice.m_values[i * m_strideX + m_strideX - 1]);
+        column.push_back(m_lowerBound[m_strideX - 1]);
+
+        return column;
+    }
+
+    void lifeStep()
+    {
+        std::vector<values_t> nextStepSliceValues(m_slice.m_values);
+
+        for(size_t x = 1; x <= m_strideX; ++x)
+            for(size_t y = 1; y <= m_strideY; ++y)
+            {
+                size_t aliveNeighbors = 0;
+                for(size_t nx = x - 1; nx < x + 2; ++nx)
+                    for(size_t ny = y - 1; ny < y + 2; ++ny)
+                    {
+                        if(nx == x && ny == y)
+                            continue;
+                        if(value(nx, ny))
+                           ++aliveNeighbors;
+                    }
+                if(value(x, y))
+                {
+                    if(aliveNeighbors < 2 || aliveNeighbors > 3)
+                        nextStepSliceValues[m_strideX * (y - 1) + x - 1] = 0;
+                }
+                else
+                {
+                    if(aliveNeighbors == 3)
+                        nextStepSliceValues[m_strideX * (y - 1) + x - 1] = 1;
+                }
+            }
+
+        m_slice.m_values = nextStepSliceValues;
+    }
+
+    Slice& m_slice;
+    const size_t m_strideX;
+    const size_t m_strideY;
+    std::vector<values_t> m_upperBound;
+    std::vector<values_t> m_lowerBound;
+    std::vector<values_t> m_leftExtendedBound;
+    std::vector<values_t> m_rightExtendedBound;
+};
+
+#endif // EXTENDEDSLICE_H
---
+++ LabM2/slice.h
@@ -0,0 +1,115 @@
+#ifndef SLICE_H
+#define SLICE_H
+
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+typedef char values_t;
+#define MPI_VALUES_TYPE MPI_CHAR
+
+/*!
+ * \brief Кусок поля
+ */
+struct Slice
+{
+    Slice():
+        m_globalX{0},
+        m_globalY{0},
+        m_stride{0}
+    {}
+
+    Slice(size_t globalX, size_t globalY, size_t stride):
+        m_globalX{globalX},
+        m_globalY{globalY},
+        m_stride{stride}
+    {}
+
+    static Slice makeRandomSlice(size_t globalX, size_t globalY, size_t strideX, size_t strideY)
+    {
+        Slice slice(globalX, globalY, strideX);
+        slice.m_values.resize(strideX * strideY, 0);
+        for(auto& value : slice.m_values)
+        {
+            int randomval = rand() % 10;
+            if(randomval > 7)
+                value = 1;
+        }
+
+        return slice;
+    }
+
+    static Slice makeZeroSlice(size_t globalX, size_t globalY, size_t strideX, size_t strideY)
+    {
+        Slice slice(globalX, globalY, strideX);
+        slice.m_values.resize(strideX * strideY, 0);
+        return slice;
+    }
+
+    size_t m_globalX;
+    size_t m_globalY;
+    size_t m_stride;
+    std::vector<values_t> m_values;
+};
+
+/*!
+ * \brief Поле (разбитое на куски)
+ */
+struct Field
+{
+    Field():
+        m_width{0},
+        m_height{0}
+    {}
+
+    Field(size_t width, size_t height):
+        m_width{width},
+        m_height{height}
+    {}
+
+    size_t m_width;
+    size_t m_height;
+    std::vector<Slice> m_slices;
+};
+
+bool operator==(const Slice& lhs, const Slice& rhs)
+{
+    return lhs.m_globalX==rhs.m_globalX &&
+           lhs.m_globalY==rhs.m_globalY &&
+           lhs.m_stride==rhs.m_stride;
+}
+
+bool operator==(const Field& lhs, const Field& rhs)
+{
+    return lhs.m_width==rhs.m_width &&
+           lhs.m_height==rhs.m_height;
+}
+
+namespace boost
+{
+    namespace serialization
+    {
+        template<class Archive>
+        void serialize(Archive& ar, Slice& slice, const unsigned int version)
+        {
+            ar & slice.m_globalX;
+            ar & slice.m_globalY;
+            ar & slice.m_stride;
+            ar & slice.m_values;
+        }
+
+        template<class Archive>
+        void serialize(Archive& ar, Field& field, const unsigned int version)
+        {
+            ar & field.m_width;
+            ar & field.m_height;
+            ar & field.m_slices;
+        }
+    }
+}
+
+#endif // SLICE_H
